a72b6e84c6e838c91295cbf57ed52e9d299a05f6
jaydeland <jaydeland@users.noreply.github.com>
2020-07-23 16:42:16 +0000

    deploy: e78c609a5c5dc7c01a906c9c21dc4bad9d98d13f

---
+++ docs/js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $('.header__menu-toggle, .sidebar__menu-toggle').on('click', function(){
+        $(this).next().slideToggle();
+    })
+
+    $('.sub-menu__toggle').on('click', function(){
+        $(this).parent().next('.sub-menu').toggle();
+        $(this).toggleClass('fa-plus-square-o fa-minus-square-o');
+    });
+
+    $('.notification-bar__close').on('click', function(){
+       $(".notification-bar").addClass("closed");
+    });
+});
